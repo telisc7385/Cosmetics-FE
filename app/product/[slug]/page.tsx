1564aa209b3cbb5281ce39b5f76760d259c4a54f
siddhant-co <your-new-email@example.com>
2025-07-10 10:45:00 +0530

    "change in product slug"

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -11,7 +11,6 @@ type Props = {
 export default async function ProductPage({ params }: Props) {
   const { slug } = await params;
 
-  console.log("Slug:", slug);
 
   const product = await fetchProductBySlug(slug);
 
@@ -19,16 +18,17 @@ export default async function ProductPage({ params }: Props) {
 
   // Ensure categoryId is available from the product
   const categoryId = product.category?.id;
+
   const relatedProducts = categoryId ? await fetchCategoryById(categoryId) : [];
 
-  // Exclude the current product
+ // Exclude the current product
   const filteredRelated = relatedProducts.filter((p) => p.id !== product.id);
 
   return (
     <div>
       <ProductDetailClient
         product={product}
-        relatedProducts={filteredRelated}
+        relatedProducts={filteredRelated }
       />
     </div>
   );
