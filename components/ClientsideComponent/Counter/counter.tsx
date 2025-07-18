6102d74a84c6138e592edcef7559a9da7a2895e8
Ashish-M-Sharma <a.m.sharma2501@gmail.com>
2025-07-18 10:54:46 +0530

    "changes heading and subheading"

--- components/ClientsideComponent/Counter/counter.tsx
+++ components/ClientsideComponent/Counter/counter.tsx
@@ -30,8 +30,8 @@ const Counter = () => {
       {/* Section header shown only once at top */}
       <div className="px-[20px] mt-2 max-w-7xl mx-auto mb-8 text-center">
         <SectionHeader
-          title="Shop Our Best Sellers"
-          subtitle="Trusted by Thousands, Loved for a Reason."
+          title="Our Company Progress"
+          subtitle="Track how we’ve grown over time."
           titleClass="text-2xl sm:text-3xl lg:text-4xl"
           subtitleClass="text-sm sm:text-base lg:text-lg"
         />
